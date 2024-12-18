b1fc3f52fc637379dcc7700eebf83f803e77e258
WillBorysiak <will.boz@icloud.com>
2024-12-18 14:13:41 +0000

    adding hardcoded origins to middleware

--- app/database/config.py
+++ app/database/config.py
@@ -25,7 +25,7 @@ origins = ["https://www.sprechedeutsch.com"]
 middleware = [
     Middleware(
         CORSMiddleware,
-        allow_origins="*",
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
