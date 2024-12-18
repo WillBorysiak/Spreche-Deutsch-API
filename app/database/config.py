d5146a27da7579e4b94a1a4eb43e26505f4b5acc
WillBorysiak <will.boz@icloud.com>
2024-12-18 13:48:03 +0000

    updated packages

--- app/database/config.py
+++ app/database/config.py
@@ -1,16 +1,17 @@
-from pydantic import BaseSettings, AnyHttpUrl
+from pydantic import AnyHttpUrl, Field
+from pydantic_settings import BaseSettings
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 
 class Settings(BaseSettings):
-    environment: str
-    db_hostname: str
-    db_port: str
-    db_username: str
-    db_password: str
-    db_name: str
-    cors_origin: AnyHttpUrl
+    environment: str = Field(validation_alias="ENVIRONMENT")
+    db_hostname: str = Field(validation_alias="DB_HOSTNAME")
+    db_port: str = Field(validation_alias="DB_PORT")
+    db_username: str = Field(validation_alias="DB_USERNAME")
+    db_password: str = Field(validation_alias="DB_PASSWORD")
+    db_name: str = Field(validation_alias="DB_NAME")
+    cors_origin: AnyHttpUrl = Field(validation_alias="CORS_ORIGIN")
 
     class Config:
         env_file = ".env"
--- migrations/env.py
+++ migrations/env.py
@@ -4,8 +4,8 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from app.config import settings
-from app.database import Base
+from app.database.config import settings
+from app.database.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
